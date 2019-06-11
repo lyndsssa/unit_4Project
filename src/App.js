e45f6ab9b312c388ab1c324a1b44669b7876e2d8
lyndsey ajster <lyndsssa@yahoo.com>
2019-06-10 18:57:42 -0700

    not sure why its making me commit when I did not change anything

--- src/App.js
+++ src/App.js
@@ -27,7 +27,6 @@ class App extends Component {
 
   }
   fetchItems() {
-<<<<<<< HEAD
   fetch('https://grocery-backend-api.herokuapp.com/items')
    .then( data => data.json())
    .then( jData => {
@@ -49,40 +48,6 @@ handleCreateTask(item) {
     headers: {
       'Accept': 'application/json, text/plain, */*',
       'Content-Type': 'application/json'
-=======
-      fetch('https://grocery-backend-api.herokuapp.com/items')
-       .then( data => data.json())
-       .then( jData => {
-         console.log('this is jData', jData)
-         this.sortItems(jData)
-       })
-    }
-
-    handleView(view) {
-        console.log('Inside App:handleView: ', view)
-        this.setState({
-            currentView: view,
-        })
-    }
-
-    // adding updating items
-    handleCreateTask(item) {
-        console.log('Inside handleCreateTask (item): ', item)
-      fetch('https://grocery-backend-api.herokuapp.com/items', {
-        body:JSON.stringify(item),
-        method:'POST',
-        headers: {
-          'Accept': 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
-        }
-      })
-        .then( addedItem => addedItem.json())
-        .then( jData => {
-          this.updateArray(jData[0], 'itemsToGet')
-          this.handleView('toGet')
-        })
-        .catch( err => console.log('this is err', err))
->>>>>>> d9e86b98e6932cade7f3c318eb0ac449789a7d75
     }
   })
     .then( addedItem => addedItem.json())
@@ -98,15 +63,7 @@ handleCreateTask(item) {
 // handle checking of item
   handleCheck(items, arrayIndex, currentArray){
     // this toggles the completed value
-<<<<<<< HEAD
     items.purchased = !items.purchased
-=======
-    console.log('purchased bool before: ', item.purchased)
-    item.purchased === 'f' ? item.purchased = true : item.purchased = false
-    // item.purchased = !item.purchased
-    console.log('purchased bool AFTER: ', item.purchased)
-    console.log('Inside App:handleCheck (item): ', item)
->>>>>>> d9e86b98e6932cade7f3c318eb0ac449789a7d75
     // now we make our fetch call to PUT (update)
     fetch('https://grocery-backend-api.herokuapp.com/items' + items.id, {
       body:JSON.stringify(items),
@@ -118,20 +75,13 @@ handleCreateTask(item) {
     })
     .then( updatedItem => updatedItem.json())
     .then(jData => {
-        console.log('Inside handleCheck (jData): ', jData)
       this.removeFromArray(currentArray, arrayIndex)
       if(currentArray === 'itemsToGet') {
-<<<<<<< HEAD
         this.updateArray(jData, 'purchasedItems')
         console.log('hi')
       } else {
         this.updateArray(jData, 'itemsToGet')
         console.log('yay')
-=======
-        this.updateArray(jData[0], 'purchasedItems')
-      } else {
-        this.updateArray(jData[0], 'itemsToGet')
->>>>>>> d9e86b98e6932cade7f3c318eb0ac449789a7d75
       }
     })
     .catch(err => console.log('this is error from handleCheck', err))
@@ -143,7 +93,6 @@ handleCreateTask(item) {
     return {
       [array]: prevState[array]
     }
-<<<<<<< HEAD
   })
   // this.setState( prevState => ({
   //   [array]: [...prevState[array].splice(arrayIndex, 1)]
@@ -167,54 +116,6 @@ handleCreateTask(item) {
       purchasedItems.push(item)
     } else { // otherwise, push it to the todoTasks array
       itemsToGet.push(item)
-=======
-
-    updateArray(task, array) {
-        console.log('Inside App:updateArray (task): ', task)
-        console.log('Inside App:updateArray (array): ', array)
-        // prevState is a copy of the currentState
-        this.setState( prevState => {
-            // PrevState['todoTasks'].push(task)
-            prevState[array].push(task)
-            console.log('Inside App:updateArray (prevState): ', prevState)
-            // We are returning an object, thus the return {}
-            return {
-                [array]: prevState[array]
-            }
-        })
-    }
-
-    // // updateArray
-    //   updateArray(task,array){
-    //   this.setState( prevState => ({
-    //     [array]:[...prevState[array],task]
-    //   }))
-    // }
-
-    // sorting items
-    sortItems(items) {
-      // default counter variables
-      let purchasedItems = []
-      let itemsToGet = []
-      // counter loop
-      items.forEach((item) => {
-        // if task is completed, push it to the completedTasks array
-        if(item.purchased === 't') {
-          purchasedItems.push(item)
-        } else { // otherwise, push it to the todoTasks array
-          itemsToGet.push(item)
-        }
-      })
-
-      this.setItems(purchasedItems, itemsToGet)
-    }
-
-    setItems(purchased, toGet) {
-    	this.setState({
-    		purchasedItems: purchased,
-    		itemsToGet: toGet
-    	})
->>>>>>> d9e86b98e6932cade7f3c318eb0ac449789a7d75
     }
   })
   this.setItems(purchasedItems, itemsToGet)
